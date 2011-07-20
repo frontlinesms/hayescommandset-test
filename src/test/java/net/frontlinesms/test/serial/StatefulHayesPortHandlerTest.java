2c19a025e1546cfa91be2aca7df366949ba54813
Alex Anderson <alex@frontlinesms.com>
2011-07-20 12:57:03 +0300

    Added tests (unimplemented).

---
+++ src/test/java/net/frontlinesms/test/serial/StatefulHayesPortHandlerTest.java
@@ -0,0 +1,19 @@
+package net.frontlinesms.test.serial;
+
+import net.frontlinesms.junit.BaseTestCase;
+
+public class StatefulHayesPortHandlerTest extends BaseTestCase {
+	private StatefulHayesPortHandler handler;
+	
+	public void test() {
+		fail("No tests written yet.");
+	}
+	
+	public void testStandardRequestMatching() {
+		fail("No tests written yet.");
+	}
+	
+	public void testRegexRequestMatching() {
+		fail("No tests written yet.");
+	}
+}
